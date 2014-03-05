807dd083df5d35a1062c0f71f028ca2493658f45
gabriel-laet <gabriel.laet@gmail.com>
2014-03-05 15:00:06 -0300

    adding System.time method

--- lib/dlapi/src/main/java/com/doubleleft/api/System.java
+++ lib/dlapi/src/main/java/com/doubleleft/api/System.java
@@ -4,8 +4,16 @@ package com.doubleleft.api;
  * Created by glaet on 2/28/14.
  */
 public class System {
+
+    protected Client client;
+
     public System(Client client)
     {
-        //TODO: implement System API
+        this.client = client;
+    }
+
+    public void time(Responder responder)
+    {
+        this.client.get("system/time", null, responder);
     }
 }
