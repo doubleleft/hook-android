b130c1b23862299149366a8b9c1b53934960380d
Tulio <tulio@doubleleft.com>
2014-11-14 17:35:15 -0200

    Added samples project

---
+++ HookSamples/src/com/doubleleft/hook/samples/MainActivity.java
@@ -0,0 +1,13 @@
+package com.doubleleft.hook.samples;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.main_activity);
+	}
+}
