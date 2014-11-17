9c17cab0f09f1052a663042282be20b5c6b5b310
Tulio <tulio@doubleleft.com>
2014-11-17 19:35:44 -0200

    Insert example with new Client setup

--- HookSamples/src/com/doubleleft/hook/samples/MainActivity.java
+++ HookSamples/src/com/doubleleft/hook/samples/MainActivity.java
@@ -1,9 +1,5 @@
 package com.doubleleft.hook.samples;
 
-import com.doubleleft.hook.Responder;
-import com.doubleleft.hook.Response;
-import com.doubleleft.hook.samples.model.Person;
-
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
@@ -12,33 +8,43 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+import com.doubleleft.hook.Client;
+import com.doubleleft.hook.Responder;
+import com.doubleleft.hook.Response;
+import com.doubleleft.hook.samples.model.Person;
+
 public class MainActivity extends Activity {
-	
+
 	private Context context;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		
+
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.main_activity);
-		
+
 		this.context = this;
-		
-		Button requestButton = (Button)this.findViewById(R.id.request_button);
+
+		// Setup hook
+		Client.appId = context.getString(R.string.hook_appId);
+		Client.appKey = context.getString(R.string.hook_appKey);
+		Client.url = context.getString(R.string.hook_endpointUrl);
+
+		Button requestButton = (Button) this.findViewById(R.id.request_button);
 		requestButton.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				
-				Person person = new Person(context);
-				person.name = "Lucas";
+
+				Person person = new Person();
+				person.name = "Tulio";
 				person.age = 26;
-				
+
 				person.create(new Responder() {
 
 					@Override
 					public void onSuccess(Response response) {
-						
+
 						Log.d("hook", response.raw);
 					}
 
@@ -46,7 +52,7 @@ public class MainActivity extends Activity {
 					public void onError(Response response) {
 						Log.d("hook", "Error creating person");
 					}
-					
+
 				});
 			}
 		});
--- HookSamples/src/com/doubleleft/hook/samples/model/Person.java
+++ HookSamples/src/com/doubleleft/hook/samples/model/Person.java
@@ -1,15 +1,9 @@
 package com.doubleleft.hook.samples.model;
 
-import android.content.Context;
-
 import com.doubleleft.hook.Entity;
 
 public class Person extends Entity {
-	
+
 	public String name;
 	public int age;
-	
-	public Person(Context context) {
-		super(context);
-	}
 }
